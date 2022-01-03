2d90e8bf2e824f775ec4f6d39c91946586fb5e95
KulthumRadi <kulthumradi94@gmail.com>
2022-01-04 00:34:21 +0200

    php task

--- index.php
+++ index.php
@@ -36,11 +36,11 @@ array_push($job , "codder")
 print_r($job)
 
 
-<!-- 4 --  Write a PHP script to sort an array using case-insensitive natural ordering -->
+<!-- prctice Q4 --  Write a PHP script to sort an array using case-insensitive natural ordering -->
 $ numbers = array ("kulthum3","Kulthum3","DeMa4","dema5")
 natcasesort($numbers)
 print_r($numbers)
-<!-- 5 -- Write a PHP script to check the largest number among three integers -->
+<!-- prctice Q5 -- Write a PHP script to check the largest number among three integers -->
 function getLargest ($n1 , $n2  , $n3 )
 {
     $max = $n1;
@@ -53,7 +53,7 @@ function getLargest ($n1 , $n2  , $n3 )
      return $max;
 }
 print_r($getLargest(20 , 30 ,40 ))
-<!-- 6-- Write a PHP script to see if a year is a leap year or not -->
+<!-- prctice Q6-- Write a PHP script to see if a year is a leap year or not -->
 function leap ($year){
     if ($year % 4 ===  0) {
         print_r("is a leap year ");
@@ -63,7 +63,7 @@ function leap ($year){
 }
 print_r(leap(1994))
 
-<!--7-- Write a PHP script using for loop to add all the integers between 0 and 30 and display the total -->
+<!--prctice Q7-- Write a PHP script using for loop to add all the integers between 0 and 30 and display the total -->
 
 function sumation (){
     $start = 0 ; 
@@ -71,4 +71,25 @@ function sumation (){
        $start +=$i 
     }
 }
+<!--  prctice  Q8 Write a PHP script that display 1-2-3-4-5-6-7-8-9-10 on one line, there will not be a hyphen in the starting anf ending position -->
+for ($x = 1; $x <= 10; $x++) {
+    if ($x !== 1) {
+        print_r("-$x");
+    } else {
+        print_r("$x");
+    }
+}
 
+
+
+
+<!-- prctice  Q9 Write a PHP script to get the first word of a sentence -->
+function get_first_word ($string){
+return $string[0]
+
+}
+
+<!-- 10-- Write a PHP script to print letters from 'a' to 'z' -->
+
+for ($x = ord('a'); $x <= ord('z'); $x++)
+    echo chr($x);
